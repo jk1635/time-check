6e7100f9d2336298f167d3fb426ff6b701229a75
jk <jkwak1635@gmail.com>
2024-01-27 17:38:08 +0900

    Fix: update remainingWorkTimeSelector to use realWorkTime

--- src/stores/selectors.ts
+++ src/stores/selectors.ts
@@ -3,23 +3,23 @@ import { selector } from "recoil";
 import { workTimeState } from "./atoms";
 import { calculateDayOffWorkTime, calculateRestTime, minutesToTime, timeToMinutes } from "../utils/timeUtils";
 
-export const totalWorkTimeMinutesSelector = selector({
-    key: "totalWorkTimeMinutesSelector",
+export const realWorkTimeMinutesSelector = selector({
+    key: "realWorkTimeSelector",
     get: ({ get }) => {
         const workTime = get(workTimeState);
-        let totalMinutes = 0;
-        workTime.forEach(dayItem => {
-            totalMinutes += calculateDayOffWorkTime(dayItem);
+        return workTime.map(dayItem => {
+            const totalWorkTimeMinutes = calculateDayOffWorkTime(dayItem);
+            const restTimeMinutes = timeToMinutes(calculateRestTime({ start: dayItem.start || "0:00", end: dayItem.end || "0:00" }));
+            return Math.max(0, totalWorkTimeMinutes - restTimeMinutes);
         });
-        return totalMinutes;
     },
 });
 
 export const remainingWorkTimeMinutesSelector = selector({
     key: "remainingWorkTimeMinutesSelector",
     get: ({ get }) => {
-        const totalWorkTimeMinutes = get(totalWorkTimeMinutesSelector);
-        return timeToMinutes("40:00") - totalWorkTimeMinutes;
+        const realWorkTimeMinutes = get(realWorkTimeMinutesSelector);
+        return timeToMinutes("40:00") - realWorkTimeMinutes.reduce((total, current) => total + current, 0);
     },
 });
 
@@ -30,17 +30,5 @@ export const overtimeStatusSelector = selector({
         return remainingWorkTimeMinutes < 0
             ? `-${minutesToTime(Math.abs(remainingWorkTimeMinutes))}`
             : minutesToTime(remainingWorkTimeMinutes);
-    },
-});
-
-export const realWorkTimeMinutesSelector = selector({
-    key: "realWorkTimeSelector",
-    get: ({ get }) => {
-        const workTime = get(workTimeState);
-        return workTime.map(dayItem => {
-            const totalWorkTimeMinutes = calculateDayOffWorkTime(dayItem);
-            const restTimeMinutes = timeToMinutes(calculateRestTime({ start: dayItem.start || "0:00", end: dayItem.end || "0:00" }));
-            return Math.max(0, totalWorkTimeMinutes - restTimeMinutes);
-        });
     },
 });
